c2410cc82b622e6191efec87ccf9b277a76e5fca
Joao Teles <joaojoseteles@gmail.com>
2023-10-19 12:06:25 -0300

    :sparkles: feat: Component GroupCard

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,12 +1,17 @@
 import { HighLinght } from "@components/HighLight";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <HighLinght title="Turmas" subTitle="Jogue com a sua turma" />
+      <GroupCard
+        title="Nome da turma"
+        onPress={() => console.log("fui clicado")}
+      />
     </Container>
   );
 }
